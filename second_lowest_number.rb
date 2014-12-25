6c91a2e37ae6c1fb2a8c44b6b3d8a5574adb5697
aldhsu <0allen_hsu@gmail.com>
2014-12-26 00:22:06 +1100

    fix(next_lowest_intagram): Enum stop iteration before hitting breakpoint.

--- second_lowest_number.rb
+++ second_lowest_number.rb
@@ -18,8 +18,8 @@ class SecondLowestNumber
 
     if result 
       if result > break_point || (result == break_point && (swap_index.nil? || digits[result] < digits[swap_index]))
-        @break_point = result
-        @lowest_index = digits[index_of_number]
+        @break_point = index_of_number
+        @swap_index = result
         return break_point
       end
     else
@@ -27,20 +27,22 @@ class SecondLowestNumber
     end
   end
 
-  def find
-    enum = (digits.length-1).downto(0).each
+  def next_intagram
+    enum = (digits.length-1).downto(1).each
 
-    while (current_index = enum.next) != break_point
+    while (current_index = enum.next) >= break_point
       find_swap_index(current_index)
     end
 
     return_number
+
   rescue StopIteration
     raise "No smaller integer next"
   end
 
   def return_number
     @digits[break_point], @digits[swap_index] = @digits[swap_index], @digits[break_point]
-    @digits
+    sorted_after = @digits[(swap_index + 1)..-1].chars.sort.join("")
+    "#{@digits[0..swap_index]}#{sorted_after}".to_i
   end
 end
--- second_lowest_number_spec.rb
+++ second_lowest_number_spec.rb
@@ -10,7 +10,7 @@ describe SecondLowestNumber do
       let(:number) { 123 }
 
       it "returns the correct number" do
-        expect(service.new(number).find_swap_index).to eq ""
+        expect(service.new(number).next_intagram).to eq 132
       end
     end
 
@@ -18,7 +18,16 @@ describe SecondLowestNumber do
       let(:number) { 132 }
 
       it "returns the correct number" do
-        expect(service.new(number).find).to eq 132
+        expect(service.new(number).next_intagram).to eq 213
+      end
+    end
+
+    context "number sequence of 321" do
+      let(:number) { 321 }
+
+      it "returns an error" do
+        expect( service.new(number).next_intagram ).to eq ""
+        # expect{ service.new(number).next_intagram }.to raise_error
       end
     end
   end
